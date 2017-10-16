6eed27e8c1afc5f69feec126e960969d4855bc64
Tobias Famulla <tobias.famulla@travelping.com>
2017-10-16 17:41:07 +0200

    add confd to strongswan image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,31 @@
+FROM golang:1.9-alpine as confd
+
+RUN apk add --no-cache make unzip wget
+RUN mkdir -p /go/src/github.com/kelseyhightower/confd && \
+  ln -s /go/src/github.com/kelseyhightower/confd /app
+
+WORKDIR /app
+
+RUN wget -O /tmp/confd.zip https://github.com/kelseyhightower/confd/archive/v0.14.0.zip && \
+    unzip -d /tmp/confd /tmp/confd.zip && \
+    cp -r /tmp/confd/*/* /app && \
+    rm -rf /tmp/confd* && \
+    make build
+
+
 FROM alpine:latest
 LABEL maintainer="tobias.famulla@travelping.com"
 
 RUN apk add --update --no-cache strongswan && \
         mkdir -p /etc/ipsec.secrets.d/ && \
-        mkdir -p /etc/ipsec.config.d/
+        mkdir -p /etc/ipsec.config.d/ && \
+        mkdir -p /etc/confd/{conf.d,templates}
+COPY --from=confd /app/bin/confd /usr/local/bin/confd
 ADD files/ipsec.conf /etc/ipsec.conf
 ADD files/ipsec.secrets /etc/ipsec.secrets
+ADD config/*.tmpl /etc/confd/templates/
+ADD files/strongswan-config.toml /etc/confd/conf.d/
+ADD files/strongswan-secret.toml /etc/confd/conf.d/
+ADD files/start-strongswan.sh /usr/local/bin
 
-CMD ipsec start --nofork
+CMD start-strongswan.sh
---
+++ files/start-strongswan.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -euo pipefail
+
+_remove_route() {
+  ip route del $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP
+  return 0
+}
+
+_term() {
+  echo "======= caught SIGTERM signal ======="
+  kill -TERM "$child" 2>/dev/null
+  wait "$child"
+  _remove_route
+  exit 0
+}
+
+_check_variables() {
+  [ -z "$IPSEC_AWS_LOCALNET" ] && { echo "Need to set IPSEC_AWS_LOCALNET"; exit 1; }
+  [ -z "$IPSEC_AWS_PSK" ] && { echo "Need to set IPSEC_AWS_PSK"; exit 1; }
+  [ -z "$IPSEC_AWS_REMOTEIP" ] && { echo "Need to set IPSEC_AWS_REMOTEIP"; exit 1; }
+  [ -z "$IPSEC_AWS_LOCALPRIVIP" ] && { echo "Need to set IPSEC_AWS_LOCALPRIVIP"; exit 1; }
+  [ -z "$IPSEC_AWS_LOCALPUBIP" ] && { echo "Need to set IPSEC_AWS_LOCALPUBIP"; exit 1; }
+  [ -z "$IPSEC_AWS_REMOTENET" ] && { echo "Need to set IPSEC_AWS_REMOTENET"; exit 1; }
+  return 0
+}
+
+trap _term SIGTERM
+
+_check_variables
+
+echo "======= Create config ======="
+confd -onetime -backend env
+echo "======= Config ======="
+cat /etc/ipsec.config.d/ipsec.aws.conf
+
+echo "======= setup route ======="
+DEFAULTROUTER=`ip route | head -1 | cut -d ' ' -f 3`
+ip route add $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP
+
+echo "======= start Strongswan ======="
+ipsec start --nofork &
+
+child=$!
+wait "$child"
+_remove_route
