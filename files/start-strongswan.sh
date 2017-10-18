5d037c4bc872d96f6c9902442e774bee519194a6
Tobias Famulla <tobias.famulla@travelping.com>
2017-10-18 12:32:02 +0200

    increase output of startup script

--- files/start-strongswan.sh
+++ files/start-strongswan.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 _remove_route() {
+  echo "ip route del $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP"
   ip route del $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP
   return 0
 }
@@ -35,6 +36,7 @@ cat /etc/ipsec.config.d/ipsec.aws.conf
 
 echo "======= setup route ======="
 DEFAULTROUTER=`ip route | head -1 | cut -d ' ' -f 3`
+echo "ip route add $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP"
 ip route add $IPSEC_AWS_LOCALNET via $DEFAULTROUTER dev eth0 proto static src $IPSEC_AWS_LOCALPRIVIP
 
 echo "======= start Strongswan ======="
